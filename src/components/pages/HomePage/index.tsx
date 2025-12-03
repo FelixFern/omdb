406bbfe7b0fd86d4a824c69d232d11b3bc232c25
felixfern <felixzhuang1@gmail.com>
2025-12-03 21:13:24 +0700

    chore: css fix

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -6,22 +6,26 @@ const HomePageContent = () => {
   const { search, handleSearch, searchOptions } = useHomePageContentImpl();
 
   return (
-    <div className="flex flex-col gap-4 mx-auto container px-4 relative">
-      <div className="sticky top-0 py-8 z-10 bg-background space-y-2">
-        <div className="text-left flex flex-col gap-1">
-          <h1 className="text-3xl font-bold text-primary">OMDB Movie</h1>
-          <p className="text-xl text-secondary-foreground">
-            Search for movies and get detailed information about them.
-          </p>
+    <div className="flex flex-col gap-4 px-4 relative">
+      <div className="mx-auto relative w-full items-center flex flex-col gap-4">
+        <div className="sticky top-0 py-8 z-10 bg-background space-y-2 w-full">
+          <div className="container mx-auto">
+            <div className="text-left flex flex-col gap-1">
+              <h1 className="text-3xl font-bold text-primary">OMDB Movie</h1>
+              <p className="text-xl text-secondary-foreground">
+                Search for movies and get detailed information about them.
+              </p>
+            </div>
+            <AutoCompleteInput
+              onChange={handleSearch}
+              value={search}
+              options={searchOptions}
+            />
+          </div>
         </div>
-        <AutoCompleteInput
-          onChange={handleSearch}
-          value={search}
-          options={searchOptions}
-        />
-      </div>
-      <div className="h-full w-full">
-        <MovieList />
+        <div className="h-full container mx-auto">
+          <MovieList />
+        </div>
       </div>
     </div>
   );
